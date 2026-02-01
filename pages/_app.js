161635f0934e59541ec42515880df59777d4e5d7
medicalwizard02 <medicalwizard02@gmail.com>
2026-02-01 20:35:54 +0300

    Add favicon and update _app.js with Head component

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
 import '../frontend/index.css';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <title>Heart Buddy - Relationship Verification</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
